4a5343e38ed343340a7b0b53c72fc135dd39d039
beresnw <beresnew.n@gmail.com>
2023-06-21 23:47:00 +0300

    merg1

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -2,39 +2,55 @@
   <div class="frame-container">
     <div class="frame-frame">
       <img
-          src="../assets/background1912-u0d-2000w.png"
+          src="../assets/background.png"
           alt="background1912"
-          class="frame-background1"
+          class="frame-background"
       />
       <div class="frame-header">
-        <span class="frame-text" @click="$router.push('/login')"><span>авторизоваться</span></span>
-        <span class="frame-text2" @click="$router.push('/MainTasks')"><span>о проекте</span></span>
+        <span class="button_register" @click="$router.push('/register')"><span>авторизоваться</span></span>
+        <span class="button_about_project"><span>о проекте</span></span>
       </div>
-      <div class="frame-frame1">
-          <span class="frame-text7" @click="$router.push('/register')"><span>вход / регистрация</span></span>
+      <!-- <div class="frame-frame1">
+          <span class="button_register_underlogo" @click="$router.push('/register')"><span>вход / регистрация</span></span>
+      </div> -->
+
+      <div class="button_underlogo_register">
+        <a class="button" @click="$router.push('/register')">вход / регистрация</a>
+        <!-- <button type="button"> вход / регистрация </button> -->
       </div>
       <img
-          src="../assets/image29310-qutt-600h.png"
-          alt="image29310"
-          class="frame-image2"
+          src="../assets/main_logo.svg"
+          class="main_logo"
       />
+
+      <span class="text_underlogo">
+        Введение в корпоративную культуру be ITMO с помощью интерактивного курса с ачивками и системой <span class="text_underlogo_one_red_word"> уровней </span>
+      </span>
+      <!-- <span class="text_underlogo_one_red_word"> уровней </span> -->
+
       <img
-          src="../assets/image39312-agdh-200h.png"
-          alt="image39312"
+          src="../assets/о_проекте.svg"
+          class="about_project"
+      />
+
+      <img
+          src="../assets/red_element.svg"
+          class="red_element"
+      />
+
+      <img
+          src="../assets/лого_белый.svg"
           class="frame-image3"
       />
-      <img
-          src="../assets/image49314-75jl-600h.png"
-          alt="image49314"
-          class="frame-image4"
-      />
+      <span class="about_text_1">Сервис помогает новичку не запутаться в сложной системе предлагая ознакомительную экскурсию в be ITMO.</span>
+      <span class="about_text_2"> Так же элемент геймификации позволяет удержать пользователя активным. Система рекомендаций может позволить каждому открыть для себя что-то новое</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Main',
+  name: 'Frame',
   props: {},
   data() {
     return {
@@ -45,6 +61,198 @@ export default {
 </script>
 
 <style scoped>
+
+/* .button_underlogo_register {
+  position: relative;
+	border: 3px solid rgba(28, 28, 28, 1);
+	outline: none;
+	display: inline-block;
+	overflow: hidden;
+	text-align: center;
+	text-decoration: none;
+	cursor: pointer;
+	text-transform: uppercase;
+	font-size: 16px;
+	padding: 12px 16px;
+	color: #282828;
+	background: transparent;
+	text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
+	margin: 20px;
+} */
+
+.button {
+	position: absolute;
+width: 500px;
+height: 90px;
+left: 710px;
+top: 952.95px;
+
+background: #FFFFFF;
+border-radius: 50px;
+	
+	outline: none;
+	display: inline-block;
+	overflow: hidden;
+	text-align: center;
+	text-decoration: none;
+	cursor: pointer;
+	text-transform: uppercase;
+	font-size: 24px;
+	padding: 25px 16px;
+  font-family: 'Montserrat';
+
+	color: #282828;
+}
+
+.button span,
+.button i {
+	z-index: 2;
+}
+
+.button:active {
+	border-color: rgba(0, 0, 0, 0.1);
+}
+
+.button::before,
+.button::after {
+	content: "";
+	position: absolute;
+	transition: all 0.7s;
+	bottom: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	z-index: -1;
+}
+
+.button::before {
+	background: rgba(28, 28, 28, 1); 
+}
+
+.button:active::before {
+	background: rgba(28, 28, 28, 0.1);
+}
+
+a.button_underlogo_register {
+  position: absolute;
+width: 244px;
+height: 29px;
+left: 838px;
+top: 983.45px;
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 24px;
+line-height: 29px;
+text-align: center;
+
+color: #333333;
+}
+
+
+
+.text_underlogo {
+position: absolute;
+width: 827.31px;
+height: 72px;
+left: 546.34px;
+top: 842.5px;
+
+font-family: 'ALS Gorizont';
+font-style: normal;
+font-weight: 800;
+font-size: 24px;
+line-height: 24px;
+text-align: center;
+
+color: #FFFFFF;
+}
+
+.text_underlogo_one_red_word {
+  /* position: absolute;
+width: 128px;
+height: 24px;
+left: 973.63px;
+top: 890.5px;
+
+font-family: 'ALS Gorizont';
+font-style: normal;
+font-weight: 800;
+font-size: 24px;
+line-height: 24px;
+
+text-align: center; */
+
+color: #EE215B;
+
+}
+
+.red_element {
+  
+position: absolute;
+width: 532.07px;
+height: 532.07px;
+left: 1344.42px;
+top: 1185.37px;
+}
+
+.about_project {
+  position: absolute;
+width: 662px;
+height: 200px;
+left: 69.56px;
+top: 1146.14px;
+
+font-family: 'ALS Gorizont';
+font-style: normal;
+font-weight: 800;
+font-size: 128px;
+line-height: 100px;
+/* or 78% */
+
+
+
+}
+
+.about_text_1 {
+  position: absolute;
+width: 1296.67px;
+height: 190px;
+left: 78.61px;
+top: 1426.4px;
+
+font-family: 'ALS Gorizont';
+font-style: normal;
+font-weight: 800;
+font-size: 24px;
+line-height: 38px;
+/* or 158% */
+
+
+color: #FFFFFF;
+
+}
+
+.about_text_2 {
+  position: absolute;
+width: 1296.67px;
+height: 190px;
+left: 78.61px;
+top: 1576.4px;
+
+font-family: 'ALS Gorizont';
+font-style: normal;
+font-weight: 800;
+font-size: 24px;
+line-height: 38px;
+/* or 158% */
+
+
+color: #FFFFFF;
+
+}
+
 .frame-container {
   width: 100%;
   display: flex;
@@ -61,32 +269,33 @@ export default {
   align-items: flex-start;
   flex-shrink: 0;
 }
-.frame-background1 {
+.frame-background {
   top: 0px;
   left: 0px;
   width: 100%;
+  
   position:absolute;
-  height: 170vh;
-  color:#141414;
+  height: 250vh;
+  color:#151212;
 }
 .frame-header {
   top: 0px;
   left: 0px;
   width: 100%;
-  height: 2vh;
+  height: 120px;
   display: flex;
   position: absolute;
   align-items: flex-start;
   flex-shrink: 1;
   background-color: #141414;
 }
-.frame-text {
-  top: 3vh;
+.button_register {
+  top: 45.5px;
   left: 80%;
   color: rgba(255, 255, 255, 1);
   height: auto;
   position: absolute;
-  font-size: 3vh;
+  font-size: 24px;
   text-align: left;
   font-family: Montserrat;
   font-weight: 500;
@@ -94,13 +303,13 @@ export default {
   font-stretch: normal;
   text-decoration: none;
 }
-.frame-text2 {
-  top: 3vh;
+.button_about_project {
+  top: 45.5px;
   left: 70%;
   color: rgba(255, 255, 255, 1);
   height: auto;
   position: absolute;
-  font-size: 3vh;
+  font-size: 24px;
   text-align: left;
   font-family: Montserrat;
   font-weight: 500;
@@ -109,33 +318,35 @@ export default {
   text-decoration: none;
 }
 .frame-frame1 {
-  top: 90vh;
-  left: 35%;
-  width: 30%;
-  height: 90px;
-  display: flex;
   position: absolute;
-  align-items: flex-start;
-  flex-shrink: 1;
-  background-color: rgba(255, 255, 255, 1);
-}
-.frame-text7 {
-  top: 30.5px;
-  left: 28%;
-  color: rgba(51, 51, 51, 1);
-  height: auto;
-  position: absolute;
-  font-size: 24px;
+width: 500px;
+height: 90px;
+left: 710px;
+top: 952.95px;
 
-  text-align: center;
-  font-family: Montserrat;
-  font-weight: 500;
-  line-height: normal;
-  font-stretch: normal;
-  text-decoration: none;
+background: #FFFFFF;
+border-radius: 50px;
+z-index: 1;
 }
-.frame-image2 {
-  top: 20vh;
+.button_register_underlogo {
+  position: absolute;
+width: 244px;
+height: 29px;
+left: 838px;
+top: 983.45px;
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 24px;
+line-height: 29px;
+text-align: center;
+z-index: 2;
+
+color: #333333;
+}
+.main_logo {
+  top: 320px;
   left: 13%;
   width: 73%;
   height: 527px;
